71b656217bf01e44854b4868af81d0c03e900db1
shin5ok <shin5ok@55mp.com>
2021-08-08 23:18:27 +0900

    like working well as the start

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 import grpc
-import urlmap_pb2
+import sys
+sys.path.append("pb")
+import pb.urlmap_pb2_grpc
+import pb.urlmap_pb2
 
 app = Flask(__name__)
 
-@app.route('/ping')
+grpc_host = os.environ.get('GRPC_HOST', 'localhost:8080')
+
+@app.route('/Ping')
 def root():
-    return "pong"
+    return "Pong"
+
+@app.route('/<path>')
+def get_org(path):
+	print(path)
+	channel = grpc.insecure_channel(grpc_host)
+	stub = pb.urlmap_pb2_grpc.RedirectionStub(channel)
+	req = pb.urlmap_pb2.RedirectPath(path=path)
+	org = stub.GetOrgByPath(req)
+	return redirect(org.org)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+
