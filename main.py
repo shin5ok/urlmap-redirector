12bccceb1406763324553a6bba76fd9bdceb0bee
shin5ok <shin5ok@55mp.com>
2021-08-08 22:14:51 +0900

    modify

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask
+import grpc
+import urlmap_pb2
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/ping')
 def root():
     return "pong"
-
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
