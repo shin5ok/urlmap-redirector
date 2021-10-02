b99650bbef83ef18028197db6fd540fc06743636
Shingo <kawanos@google.com>
2021-10-03 00:55:01 +0900

    delete unneccesary char

--- secretm.py
+++ secretm.py
@@ -1,6 +1,7 @@
 from google.cloud import secretmanager
 
-class Secretm():
+class Secretm:
+
     def __init__(self, project):
         self.project = project
         self.client = secretmanager.SecretManagerServiceClient()
@@ -8,6 +9,10 @@ class Secretm():
     def get(self, secret_id, version_id="latest"):
         project_id = self.project
         name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-        response = self.client.access_secret_version(request={"name": name})
-        payload = response.payload.data.decode("UTF-8")
-        return payload
+        try:
+            response = self.client.access_secret_version(request={"name": name})
+            payload = response.payload.data.decode("UTF-8")
+            return payload
+        except Exception as e:
+            print(e)
+            return ""
