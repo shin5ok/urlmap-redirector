79b50f92f46618141f6fc01344d83443ecb8eaec
Shingo <kawanos@google.com>
2021-10-28 09:42:34 +0900

    modify

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ topic_id = os.environ.get('TOPIC_ID')
 channel = grpc.insecure_channel(grpc_host)
 stub = pb.urlmap_pb2_grpc.RedirectionStub(channel)
 
-version = "0.12"
+version = "0.60"
 
 fail_site_path = "/Failure"
 
