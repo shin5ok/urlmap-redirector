3de4656f3601a6164608af1506d831575f5c131e
Shingo <kawanos@google.com>
2021-11-07 13:27:50 +0900

    fix the lack of config to bind KSA with GSA

--- utils/workload-identity.sh
+++ utils/workload-identity.sh
@@ -3,3 +3,4 @@ gcloud iam service-accounts create urlmap-redirector
 gcloud projects add-iam-policy-binding $PROJECT     --member "serviceAccount:urlmap-redirector@$PROJECT.iam.gserviceaccount.com"     --role "roles/secretmanager.secretAccessor"
 gcloud projects add-iam-policy-binding $PROJECT     --member "serviceAccount:urlmap-redirector@$PROJECT.iam.gserviceaccount.com"     --role "roles/pubsub.publisher"
 gcloud iam service-accounts add-iam-policy-binding     --role roles/iam.workloadIdentityUser     --member "serviceAccount:$PROJECT.svc.id.goog[urlmap/urlmap-redirector]"     urlmap-redirector@$PROJECT.iam.gserviceaccount.com
+kubectl annotate serviceaccount --namespace urlmap urlmap-redirector iam.gke.io/gcp-service-account=urlmap-redirector@$PROJECT.iam.gserviceaccount.com
