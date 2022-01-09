52b68173a7ab58eb789f15428de613b1f44373ce
Shingo <kawanos@google.com>
2022-01-10 02:03:06 +0900

    make it versatilile use as class

--- mygcplogging.py
+++ mygcplogging.py
@@ -2,8 +2,15 @@ import logging
 import sys
 from google.cloud.logging.handlers import ContainerEngineHandler
 
-def init(name: str):
-    logger = logging.getLogger(name)
-    logger.addHandler(ContainerEngineHandler(name=name, stream=sys.stdout))
-    logger.propagate = False
-    return logger
+class GCPLog:
+
+    def init(self, name: str):
+        logger = logging.getLogger(name)
+        logger.addHandler(ContainerEngineHandler(name=name, stream=sys.stdout))
+        logger.propagate = False
+
+    def info(self, logdict):
+        pass
+
+    def _put(self, level, logdict):
+        pass
