ee8f4c9cbb28fe6261de510d01f9db1f0b5450f1
Shingo <kawanos@google.com>
2022-01-10 19:26:52 +0900

    implement more constructor and some stuff

--- mygcplogging.py
+++ mygcplogging.py
@@ -4,13 +4,27 @@ from google.cloud.logging.handlers import ContainerEngineHandler
 
 class GCPLog:
 
-    def init(self, name: str):
+    def __init__(self, name: str, mode: str) -> None:
         logger = logging.getLogger(name)
-        logger.addHandler(ContainerEngineHandler(name=name, stream=sys.stdout))
+        if mode == "gke":
+            logger.addHandler(ContainerEngineHandler(name=name, stream=sys.stdout))
         logger.propagate = False
+        self.mode = name
+        self.mode = mode
+        self.logger = logger
+
+    def mode(self, mode: str=""):
+        # mode can has a value like gke or run
+        if not mode:
+            self.mode = mode
+        return mode
 
     def info(self, logdict):
-        pass
+        self._put("info", logdict)
 
     def _put(self, level, logdict):
-        pass
+        import json
+        try:
+            self.logger.info(json.dumps(logdict))
+        except Exception as e:
+            print(str(e))
