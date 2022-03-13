125835a1ed5135b8e4999a5d748c8ef1b1a8b114
Shingo <kawanos@google.com>
2022-03-14 01:06:50 +0900

    set handler corresponding to multi signals

--- shoutout.py
+++ shoutout.py
@@ -1,14 +1,31 @@
+import os
+import sys
+import json
 import signal
 import requests
-import os
-import json
 
-def set_shoutout_when_catching_signal(slack_url: str, json_dict: dict) -> None:
-    payload = json.dumps(json_dict)
+def set_shoutout_when_catching_signal(slack_url: str, data_dict: dict) -> None:
+
     def _handler(sig: int, frame) -> None:
+        hostname = _get_where_i_am()
+        data_dict["text"] = f"{hostname}:signal {sig}"
+        payload = json.dumps(data_dict)
         requests.post(slack_url, data=payload)
-    signal.signal(signal.SIGTERM, _handler)
+        sys.exit(0)
 
+    def _get_where_i_am() -> str:
+        metadata_url = "http://169.254.169.254/computeMetadata/v1/instance/hostname"
+        try:
+            res = requests.get(metadata_url, headers={"Metadata-Flavor":"Google"}, timeout=(0.5,0.5))
+            data = res.content
+        except Exception as e:
+            print(str(e))
+            data = os.uname()[1]
+        return data
+
+    signal.signal(signal.SIGTERM, _handler)
+    signal.signal(signal.SIGINT, _handler)
+    signal.signal(signal.SIGHUP, _handler)
 
 if __name__ == '__main__':
     set_shoutout_when_catching_signal(os.environ.get("SLACK_URL"),{"text":"test", "channel":"#kawano"})
